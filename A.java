b135b76bf6741a566fd61ca11685df0a9eae9e6b
yaswanthyashu1122 <113525100+yaswanthyashu1122@users.noreply.github.com>
2022-09-14 18:41:51 +0530

    my first uploaded file
    my first java file

---
+++ A.java
@@ -0,0 +1,15 @@
+class A
+{
+	public static void main(String[] args) 
+	{
+		for(int i =1; i<=2; i++)
+			{
+	    for(int j =1; j<=8; j++)
+			{
+		System.out.print(1 + " ");
+		}
+		System.out.println();
+		}
+
+	}
+}
